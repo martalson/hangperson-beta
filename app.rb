f9294d5995218ad187feba077a08ccba047a3aa8
martalson <martinalonso@berkeley.edu>
2015-02-09 00:42:00 -0800

    to autograder

--- app.rb
+++ app.rb
@@ -39,7 +39,9 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    if not @game.guess(letter)
+      flash[:message] = "You have already used that letter"
+    end
     redirect '/show'
   end
   
@@ -49,18 +51,27 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use instance variables
   # @wrong_guesses and @word_with_guesses, so set those up here.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    state = @game.check_win_or_lose
+    if state == :win
+      redirect '/win'
+    elsif state == :lose
+      redirect '/lose'
+    end
+    erb :show 
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose != :win or not @game
+      redirect '/show'
+    end
+    erb :win 
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose != :lose or not @game
+      redirect '/show'
+    end
+    erb :lose 
   end
   
 end
